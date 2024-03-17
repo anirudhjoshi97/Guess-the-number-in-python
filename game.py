0f5afae53ef2da2691d1aa041565a78bf3cc5f7c
Anirudh Joshi <anirudh.joshi76@gmail.com>
2024-03-17 22:59:30 +0530

    new update in the game

--- game.py
+++ game.py
@@ -7,23 +7,28 @@ c = "scissor"
 print("Enter a, b, or c")
 user_choice = input("Enter your action: ")
 
-if user_choice == a:
+if user_choice == 'a':
     print("You have chosen", a)
-elif user_choice == b:
+elif user_choice == 'b':
     print("You have chosen", b)
-elif user_choice == c:
+elif user_choice == 'c':
     print("You have chosen", c)
 else:
-    print("Invalid choice")
+    print("Invalid input")
+    exit()
 
 computer_choice = random.choice([a, b, c])
 print("The computer has chosen", computer_choice)
 
 if user_choice == computer_choice:
     print("It's a draw")
-elif (user_choice == a and computer_choice == b) or \
-     (user_choice == b and computer_choice == c) or \
-     (user_choice == c and computer_choice == a):
+elif (user_choice == 'a' and computer_choice == b):
+     print("the computer has won the game")
+
+elif (user_choice == 'b' and computer_choice == c):
+     print("the computer has won the game")
+
+elif(user_choice == 'c' and computer_choice == a):
     print("Computer has won the game")
 else:
     print("You have won the game")
